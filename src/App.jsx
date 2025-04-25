d9855c9a12e49efa164e84ac8bcc908fffe0e611
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-04-25 21:31:13 +0100

    Synchronize the input of the General Infos with displaying them

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import "./App.css";
-import { Infos } from "./components/Infos";
-import { Resume } from "./components/Resume";
+import { General } from "./components/General";
+import { Experience } from "./components/Experience";
+import { Education } from "./components/Education";
+import { useState } from "react";
 
 function App() {
+	const [generalInfos, setGeneralInfos] = useState({
+		name: "Oussama Lejmi",
+		phone: "0021629147740",
+		email: "oussama.lejmi@outlook.com",
+	});
 	return (
 		<>
-			<Infos />
-			<Resume />
+			<div className="infos">
+				<General
+					name={generalInfos.name}
+					phone={generalInfos.phone}
+					email={generalInfos.email}
+					handleChange={setGeneralInfos}
+				/>
+				<Education />
+				<Experience />
+			</div>
+
+			<div className="resume">
+				<div className="general">
+					<h1>{generalInfos.name}</h1>
+					<div style={{ display: "flex", justifyContent: "space-around", fontSize: "26px" }}>
+						<span>{generalInfos.phone}</span> <span>{generalInfos.email}</span>
+					</div>
+				</div>
+				<div className="education">
+					<h2>Educational Experience</h2>
+				</div>
+				<div className="experience">
+					<h2>Practice Experience</h2>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,26 +1,60 @@
-export function General() {
+export function General({ name, email, phone, handleChange }) {
+	const tempInfo = {
+		name,
+		email,
+		phone,
+	};
 	return (
 		<div className="general">
 			<h2>General Information</h2>
-			<form>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					handleChange({
+						name: tempInfo.name,
+						phone: tempInfo.phone,
+						email: tempInfo.email,
+					});
+				}}
+			>
 				<div>
 					<label htmlFor="name">Name</label>
 					<br />
-					<input type="text" id="name" />
+					<input
+						type="text"
+						id="name"
+						defaultValue={name}
+						onChange={(e) => {
+							tempInfo.name = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
 					<label htmlFor="email">Email</label>
 					<br />
-					<input type="email" id="email" />
+					<input
+						type="email"
+						defaultValue={email}
+						id="email"
+						onChange={(e) => {
+							tempInfo.email = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
 					<label htmlFor="phone">Phone</label>
 					<br />
-					<input type="tel" id="phone" />
+					<input
+						type="tel"
+						defaultValue={phone}
+						id="phone"
+						onChange={(e) => {
+							tempInfo.phone = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
-					<button>Edit</button>
-					<button>Submit</button>
+					<button type="submit">Submit</button>
 				</div>
 			</form>
 		</div>
