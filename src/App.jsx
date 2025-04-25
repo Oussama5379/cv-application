1ec9758f0ab2c3b262278d38db9b1f557d2d7fab
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-04-25 22:31:02 +0100

    Enable displaying the infos about Work Experience

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ function App() {
 		phone: "0021629147740",
 		email: "oussama.lejmi@outlook.com",
 	});
+	const [experienceInfos, setExperienceInfos] = useState({
+		companyName: "Google",
+		positionTitle: "Software Engineer",
+		dateOfWork: "September 2023 - Present",
+	});
 	return (
 		<>
 			<div className="infos">
@@ -19,8 +24,13 @@ function App() {
 					email={generalInfos.email}
 					handleChange={setGeneralInfos}
 				/>
+				<Experience
+					companyName={experienceInfos.companyName}
+					positionTitle={experienceInfos.positionTitle}
+					dateOfWork={experienceInfos.dateOfWork}
+					handleChange={setExperienceInfos}
+				/>
 				<Education />
-				<Experience />
 			</div>
 
 			<div className="resume">
@@ -30,11 +40,22 @@ function App() {
 						<span>{generalInfos.phone}</span> <span>{generalInfos.email}</span>
 					</div>
 				</div>
+				<div className="experience">
+					<h1>Practice Experience</h1>
+					<h3></h3>
+					<h4></h4>
+					<p>
+						<span style={{ fontWeight: "600", fontSize: "26px " }}>
+							{experienceInfos.companyName}
+						</span>
+					</p>
+					<div style={{ display: "flex", justifyContent: "space-between", fontSize: "22px" }}>
+						<span>{experienceInfos.positionTitle + " "}</span>
+						<span>{experienceInfos.dateOfWork}</span>
+					</div>
+				</div>
 				<div className="education">
 					<h2>Educational Experience</h2>
-				</div>
-				<div className="experience">
-					<h2>Practice Experience</h2>
 				</div>
 			</div>
 		</>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,25 +1,59 @@
-export function Experience() {
+export function Experience({ companyName, positionTitle, dateOfWork, handleChange }) {
+	const tempInfo = {
+		companyName,
+		positionTitle,
+		dateOfWork,
+	};
 	return (
 		<div className="experience">
 			<h2>Practice Experience</h2>
-			<form>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					handleChange({
+						companyName: tempInfo.companyName,
+						positionTitle: tempInfo.positionTitle,
+						dateOfWork: tempInfo.dateOfWork,
+					});
+				}}
+			>
 				<div>
 					<label htmlFor="companyName">Company Name</label>
 					<br />
-					<input type="text" id="companyName" />
+					<input
+						type="text"
+						defaultValue={companyName}
+						id="companyName"
+						onChange={(e) => {
+							tempInfo.companyName = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
 					<label htmlFor="positionTitle">Position Title </label>
 					<br />
-					<input type="text" id="positionTitle" />
+					<input
+						type="text"
+						defaultValue={positionTitle}
+						id="positionTitle"
+						onChange={(e) => {
+							tempInfo.positionTitle = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
 					<label htmlFor="dateOfWork">Date Range</label>
 					<br />
-					<input type="text" id="dateOfWork" placeholder="September 2023 - Present" />
+					<input
+						type="text"
+						defaultValue={dateOfWork}
+						id="dateOfWork"
+						onChange={(e) => {
+							tempInfo.dateOfWork = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
-					<button>Edit</button>
 					<button>Submit</button>
 				</div>
 			</form>
