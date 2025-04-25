79e4f17b7a1550afc5b2f1732d81c6844429e324
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-04-25 22:56:48 +0100

    Enable adding educational infos

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ function App() {
 	const [experienceInfos, setExperienceInfos] = useState({
 		companyName: "Google",
 		positionTitle: "Software Engineer",
-		dateOfWork: "September 2023 - Present",
+		dateOfWork: "September 2024 - Present",
+	});
+	const [educationInfos, setEducationInfos] = useState({
+		schoolName: "INSAT",
+		titleOfStudy: "Software Engineering",
+		dateOfStudy: "September 2023 - Present",
 	});
 	return (
 		<>
@@ -30,7 +35,12 @@ function App() {
 					dateOfWork={experienceInfos.dateOfWork}
 					handleChange={setExperienceInfos}
 				/>
-				<Education />
+				<Education
+					schoolName={educationInfos.schoolName}
+					titleOfStudy={educationInfos.titleOfStudy}
+					dateOfStudy={educationInfos.dateOfStudy}
+					handleChange={setEducationInfos}
+				/>
 			</div>
 
 			<div className="resume">
@@ -42,8 +52,7 @@ function App() {
 				</div>
 				<div className="experience">
 					<h1>Practice Experience</h1>
-					<h3></h3>
-					<h4></h4>
+
 					<p>
 						<span style={{ fontWeight: "600", fontSize: "26px " }}>
 							{experienceInfos.companyName}
@@ -55,7 +64,16 @@ function App() {
 					</div>
 				</div>
 				<div className="education">
-					<h2>Educational Experience</h2>
+					<h1>Educational Experience</h1>
+					<p>
+						<span style={{ fontWeight: "600", fontSize: "26px " }}>
+							{educationInfos.schoolName}
+						</span>
+					</p>
+					<div style={{ display: "flex", justifyContent: "space-between", fontSize: "22px" }}>
+						<span>{educationInfos.titleOfStudy + " "}</span>
+						<span>{educationInfos.dateOfStudy}</span>
+					</div>
 				</div>
 			</div>
 		</>
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,25 +1,59 @@
-export function Education() {
+export function Education({ schoolName, titleOfStudy, dateOfStudy, handleChange }) {
+	const tempInfo = {
+		schoolName,
+		titleOfStudy,
+		dateOfStudy,
+	};
 	return (
 		<div className="education">
 			<h2>Educational Experience</h2>
-			<form>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					handleChange({
+						schoolName: tempInfo.schoolName,
+						titleOfStudy: tempInfo.titleOfStudy,
+						dateOfStudy: tempInfo.dateOfStudy,
+					});
+				}}
+			>
 				<div>
 					<label htmlFor="schoolName">School Name</label>
 					<br />
-					<input type="text" id="schoolName" />
+					<input
+						type="text"
+						id="schoolName"
+						defaultValue={schoolName}
+						onChange={(e) => {
+							tempInfo.schoolName = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
-					<label htmlFor="TitleOfStudy">Title Of Study </label>
+					<label htmlFor="titleOfStudy">Title Of Study </label>
 					<br />
-					<input type="text" id="TitleOfStudy" />
+					<input
+						type="text"
+						id="titleOfStudy"
+						defaultValue={titleOfStudy}
+						onChange={(e) => {
+							tempInfo.titleOfStudy = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
 					<label htmlFor="dateOfStudy">Date Range</label>
 					<br />
-					<input type="text" id="dateOfStudy" placeholder="September 2023 - Present" />
+					<input
+						type="text"
+						id="dateOfStudy"
+						defaultValue={dateOfStudy}
+						onChange={(e) => {
+							tempInfo.dateOfStudy = e.target.value;
+						}}
+					/>
 				</div>
 				<div>
-					<button>Edit</button>
 					<button>Submit</button>
 				</div>
 			</form>
